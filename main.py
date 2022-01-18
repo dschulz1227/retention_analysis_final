4bcb44ced238425a522ba533c65f860768897966
Damon Schulz <damon.schulz@gmail.com>
2022-01-18 08:08:45 -0600

    Initial commit, added brief description and art.

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Engineer a program that can analyze the data between each of the 3 shifts' camera checks in order to compare
+the data against each other.
+Find discrepancies and potential patterns that reflect the health of our network.
+Make predictions using data
+"""
+
+# import logo from art
+
+# print(logo)
+
